0973583c4f96361a25801c12b835f7196ed8e340
Min Lee <minlee@Zipcoder04s-MacBook-Pro.local>
2016-05-11 19:08:31 -0400

    SicBo in Progress

---
+++ CasinoApp/src/main/java/io/FoF/SicBoBet.java
@@ -0,0 +1,21 @@
+package io.FoF;
+
+import java.util.HashMap;
+
+/**
+ * Created by minlee on 5/11/16.
+ */
+public class SicBoBet {
+
+    private int payoutRatio;
+    private double betAmount;
+
+    public SicBoBet(double betAmount) {
+        this.betAmount = betAmount;
+    }
+
+    public booelan
+
+
+
+}
