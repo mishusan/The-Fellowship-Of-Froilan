66bf696d4745906f0b7715bd3ef33b1dabe6e697
Nurudeen Lawal <nurudeenlawal@Zipcoder14s-MacBook-Pro.local>
2016-05-11 18:23:23 -0400

    display added

---
+++ CardsDieSlots/src/main/java/io/nurudeenLawal/Cards.java
@@ -0,0 +1,21 @@
+package io.nurudeenLawal;
+
+/**
+ * Created by nurudeenlawal on 5/11/16.
+ */
+public class Cards {
+    public static aceOfSpades{
+        int value =1;
+        String face = "
+         _________
+        |A        |
+        |@   *    |
+        |   / \   |
+        |  /_@_\  |
+        |    !    |
+        |   ~ ~  @|
+        |        V|
+        ~~~~~~~~~~~
+"
+    }
+}
--- CasinoApp/src/main/java/io/FoF/BlackJackPlayer.java
+++
@@ -1,7 +0,0 @@
-package io.FoF;
-
-/**
- * Created by matthewb on 5/11/16.
- */
-public class BlackJackPlayer {
-}
