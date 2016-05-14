bfcb057dfd4046c3161e8299090f84306010a4c7
Matthew Belongia <matthewb@Zipcoder18s-MacBook-Pro.local>
2016-05-14 14:43:32 -0400

    new Slots game

--- CasinoApp/src/main/java/io/FoF/Tumbler.java
+++ CasinoApp/src/main/java/io/FoF/Tumbler.java
@@ -20,67 +20,56 @@ public class Tumbler {
     }
 
     public void setTumbler1(int tumbler1) {
-        this.tumbler1 = tumbler1;
-        if (tumbler1 > 8) {
-            this.tumbler1 -= 8;
-        }
+        this.tumbler1 = tumbler1 %4;
+
     }
 
     public void setTumbler2(int tumbler2) {
-        this.tumbler2 = tumbler2;
-        if (tumbler2 > 8) {
-            this.tumbler2 -= 8;
-        }
+        this.tumbler2 = tumbler2 %4;
+
     }
 
     public void setTumbler3(int tumbler3) {
-        this.tumbler3 = tumbler3;
-        if (tumbler3 > 8) {
-            this.tumbler3 -= 8;
-        }
+        this.tumbler3 = tumbler3 %4;
     }
 
     public void setTumbler4(int tumbler4) {
-        this.tumbler4 = tumbler4;
-        if (tumbler4 > 8) {
-            this.tumbler4 -= 8;
-        }
+        this.tumbler4 = tumbler4 %4;
+
     }
 
     public void setTumbler5(int tumbler5) {
-        this.tumbler5 = tumbler5;
-        if (tumbler5 > 8) {
-            this.tumbler5 -= 8;
-        }
+        this.tumbler5 = tumbler5 %4;
+
     }
 
     public int getTumbler1() {
-        return tumbler1;
+        return this.tumbler1;
     }
 
     public int getTumbler2() {
-        return tumbler2;
+        return this.tumbler2;
     }
 
     public int getTumbler3() {
-        return tumbler3;
+        return this.tumbler3;
     }
 
     public int getTumbler4() {
-        return tumbler4;
+        return this.tumbler4;
     }
 
     public int getTumbler5() {
-        return tumbler5;
+        return this.tumbler5;
     }
 
     public String getFace(int tumbler) {
         if (tumbler < 0) {
-            tumbler += 8;
+            tumbler = Math.abs((tumbler%4));
         }
 
-        if (tumbler > 10) {
-            tumbler -= 8;
+        if (tumbler > 3) {
+            tumbler = tumbler%4;
         }
         if (String.valueOf(tumbler).length() > 1) {
             return Integer.toString(tumbler);
