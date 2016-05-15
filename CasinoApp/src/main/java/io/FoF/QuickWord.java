670d4a214861c9089c4a5d9baf3fc839c85b7078
Matthew Belongia <matthewb@Zipcoder18s-MacBook-Pro.local>
2016-05-15 12:45:05 -0400

    quickWord multithread

--- CasinoApp/src/main/java/io/FoF/QuickWord.java
+++ CasinoApp/src/main/java/io/FoF/QuickWord.java
@@ -1,73 +1,117 @@
 package io.FoF;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  * Created by matthewb on 5/14/16.
  */
-public class QuickWord extends Game{
+public class QuickWord extends Game implements Runnable {
+    private Thread t;
+    private String threadQuickWord;
+    double pot;
+    int correctGuesses;
+    String[] strings;
+    Date date;
+    double bet;
+    static int counter;
 
+    public QuickWord() {
+        threadQuickWord = "newQuickWordThread";
+        this.pot = 0;
+        this.correctGuesses = 0;
+        this.strings = new String[21];
+        this.strings[0] = "x";
+        this.strings[1] = "y";
+        this.strings[2] = "z";
+        this.strings[3] = "x";
+        this.strings[4] = "x";
+        this.strings[5] = "z";
+        this.strings[6] = "z";
+        this.strings[7] = "y";
+        this.strings[8] = "x";
+        this.strings[8] = "q";
+        this.strings[9] = "f";
+        this.strings[10] = "e";
+        this.strings[11] = "l";
+        this.strings[12] = "l";
+        this.strings[13] = "o";
+        this.strings[14] = "w";
+        this.strings[15] = "s";
+        this.strings[16] = "h";
+        this.strings[17] = "i";
+        this.strings[18] = "p";
+        this.strings[19] = "of";
+        this.strings[20] = "Froilan";
+
+
+    }
+
+    public void run() {
+        try {
+            for (int i = 0; i < 20; i++) {
+                System.out.println(strings[i]);
+                Thread.sleep(2000);
+            }
+        } catch (InterruptedException e) {
+        }
+    }
+
+    public void start() {
+        if (t == null) {
+            t = new Thread(this, threadQuickWord);
+            t.start();
+        }
+    }
 
     public void startGame(Player player) {
-        double pot =0;
-        int correctGuesses = 0;
-        String[] strings = new String[9];
-        strings[0] = "X";
-        strings[1] = "Y";
-        strings[2] = "Z";
-        strings[3] = "X";
-        strings[4] = "X";
-        strings[5] = "Z";
-        strings[6] = "Z";
-        strings[7] = "Y";
-        strings[8] = "X";
-        double bet = Display.getDoublePrompt("How much are you betting?");
-        MoneyFromPurse(player,bet);
-        pot += bet;
+        bet = Display.getDoublePrompt("How much are you betting?");
+        if (checkAmountInPurse(player, bet)) {
+            MoneyFromPurse(player, bet);
+            pot += bet;
+        } else {
+            Display.showMessage("You don't have that kind of cash pot setting to 0");
+            pot = 0.0;
+        }
         Display.showMessage("Enter the Letters as they Appear to Get Points");
         Display.showMessage("Get ready..... Go!");
-        Display.pausedShowMessage(strings);
-        String guess1 = Display.getStringPrompt("");
-        String guess2 = Display.getStringPrompt("");
-        String guess3 = Display.getStringPrompt("");
-        String guess4 = Display.getStringPrompt("");
-        String guess5 = Display.getStringPrompt("");
-        String guess6 = Display.getStringPrompt("");
-        String guess7 = Display.getStringPrompt("");
-        String guess8 = Display.getStringPrompt("");
-        String guess9 = Display.getStringPrompt("");
 
-
-        if(guess1.equals(strings[0])){
-            correctGuesses++;
-        }
-        if(guess2.equals(strings[1])){
-            correctGuesses++;
-        }
-        if(guess3.equals(strings[2])){
-            correctGuesses++;
-        }
-        if(guess4.equals(strings[3])){
-            correctGuesses++;
-        }
-        if(guess5.equals(strings[4])){
-            correctGuesses++;
-        }
-        if(guess6.equals(strings[5])){
-            correctGuesses++;
-        }
-        if(guess7.equals(strings[6])){
-            correctGuesses++;
-        }
-        if(guess8.equals(strings[7])){
-            correctGuesses++;
-        }
-        if(guess9.equals(strings[8])){
-            correctGuesses++;
-        }
+        date = new Date();
+        QuickWord R1 = new QuickWord();
+        R1.start();
+        multiThreadLetterDisplay(R1);
 
         Display.showMessage("You got " + correctGuesses + " correct");
-        double winnings = pot*(correctGuesses/3);
+        double winnings = pot * (correctGuesses / 3);
         Display.showMessage("Your won $" + winnings);
-        MoneyToPurse(player,winnings);
+        MoneyToPurse(player, winnings);
+
+
+    }
+
+    public void multiThreadLetterDisplay(QuickWord quickWord) {
+        for (int numGuess = 0; numGuess < 22; numGuess++) {
+            Date newDate = new Date();
+            Display.showMessage(20 - (newDate.getTime() - date.getTime()) / 1000 + "seconds left");
+            if (newDate.getTime() > (date.getTime() + 20000)) {
+                Display.showMessage("Times Up!");
+                quickWord.t.interrupt();
+                break;
+            }
+            String guess = Display.getStringPrompt("");
+            try {
+                if (guess.equals(strings[numGuess])) {
+                    correctGuesses++;
+                }
+            }catch (ArrayIndexOutOfBoundsException e){
+                Display.showMessage("Out of attempts!");
+                quickWord.t.interrupt();
+                break;
+            }
+
+        }
 
     }
 }
+
+
